e3b64b0b1f002c3cb17b7fda58b4231dcd52b8b9
Valentin Alberto <valalberto0214@gmail.com>
2024-02-09 23:10:29 +0800

    initial commit

---
+++ src/main/java/com/via/vwitter/VwitterApiApplication.java
@@ -0,0 +1,13 @@
+package com.via.vwitter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VwitterApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VwitterApiApplication.class, args);
+	}
+
+}
